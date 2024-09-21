8dce1d4bd701cdd00cfd7090d9653196e4258f9a
Slayer <slayerpro27@gmail.com>
2024-09-22 00:22:25 +0300

    Proxy improvements: if some session has linked proxy, which no longer works, a new unused proxy will be picked from the list Now you can set amount of sessions, that can use same proxy

--- bot/config/config.py
+++ bot/config/config.py
@@ -21,7 +21,9 @@ class Settings(BaseSettings):
     USE_RANDOM_DELAY_IN_RUN: bool = True
     RANDOM_DELAY_IN_RUN: list[int] = [0, 30]
 
+    SESSIONS_PER_PROXY: int = 1
     USE_PROXY_FROM_FILE: bool = False
+    USE_PROXY_CHAIN: bool = False
 
     DEVICE_PARAMS: bool = False
 
