39426db7f26219cf359d8a3aa1d7dbc3981ffbd7
Slayer <slayerpro27@gmail.com>
2024-10-10 21:12:05 +0300

    Added support for .json session files

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from time import time
 from urllib.parse import unquote, quote
 
-from telethon import TelegramClient
+from opentele.tl import TelegramClient
 from telethon.errors import *
 from telethon.types import InputBotAppShortName, InputUser
 from telethon.functions import messages
@@ -53,7 +53,7 @@ class Tapper:
 
         session_config = config_utils.get_session_config(self.session_name, CONFIG_PATH)
 
-        if not all(key in session_config for key in ('api_id', 'api_hash', 'user_agent')):
+        if not all(key in session_config for key in ('api', 'user_agent')):
             logger.critical(self.log_message('CHECK accounts_config.json as it might be corrupted'))
             exit(-1)
 
